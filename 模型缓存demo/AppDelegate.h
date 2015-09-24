01c389730160c305e162767b04adacc5a50d5392
alexluan <326083325@qq.com>
2015-09-24 19:44:43 +0800

    Initial Commit

---
+++ 模型缓存demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  模型缓存demo
+//
+//  Created by 栾有数 on 15/9/24.
+//  Copyright (c) 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 模型缓存demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  模型缓存demo
+//
+//  Created by 栾有数 on 15/9/24.
+//  Copyright (c) 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
