1de8019d6063c221ebd01e4a8222ed223dfcb224
alexluan <326083325@qq.com>
2015-10-20 12:13:43 +0800

    模型缓存
    归档，反归档

--- 模型缓存demo/ViewController.m
+++ 模型缓存demo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <Foundation/Foundation.h>
 @interface ViewController ()
 
 @end
@@ -17,8 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *rootDir = NSHomeDirectory();
+    NSString *path = [rootDir stringByAppendingPathComponent:@"test.arch"];
+    
+    NSMutableData *data = [NSMutableData data];
+    NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:data];
+    
+    NSArray *array = @[@"one",@"two"];
+    [archiver encodeInt:100 forKey:@"age"];//相当于SharedPreference的putInteger(key,value);
+    [archiver encodeObject:array forKey:@"names"];//
+    [archiver finishEncoding];//此方法调用，则将数据存入data
+    
+    if([data writeToFile:path atomically:YES]){
+        NSLog(@"对象存入文件成功");
+    }
+    [self get];
+ 
 }
+-(void)get{
+    NSString *rootDir = NSHomeDirectory();
+    NSString *path = [rootDir stringByAppendingPathComponent:@"test.arch"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    NSKeyedArchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+    int age = [unarchiver decodeIntForKey:@"age"];//取出值
+    NSArray *array = [unarchiver decodeObjectForKey:@"names"];
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
